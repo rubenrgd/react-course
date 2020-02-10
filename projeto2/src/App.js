cccaf11eb0578c823384c035792d14f820b00445
F8809379 Ruben Rodrigues Goncalves Dias <rubendias@bb.com.br>
2020-02-10 15:48:30 -0300

    Dia 2

--- projeto2/src/App.js
+++ projeto2/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import Navbar from './components/navbar'
+import Rotas from './rotas'
+
 function App() {
   return (
-    <div className="alert alert-dismissible alert-warning">
-      <button type="button" classNamess="close" data-dismiss="alert">&times;</button>
-      <h4 className="alert-heading">Warning!</h4>
-      <p className="mb-0"> Hello <a href="#" className="alert-link">Bootswatch!</a>.</p>
-    </div>
+    <>
+      <div className="container">
+        <Navbar />
+        <Rotas />
+      </div>
+    </>
   );
 }
 
---
+++ projeto2/src/app/produtoService.js
@@ -0,0 +1,55 @@
+const PRODUTOS = '_PRODUTOS'
+
+
+export function ErroValidacao(errors){
+    this.errors = errors;
+}
+
+export default class ProdutoService {
+
+
+        validar = (produto) => {
+            const errors = []
+
+            if(!produto.nome){
+                errors.push('O campo nome é obrigatório.')
+            }
+
+            if(!produto.sku){
+                errors.push('O campo SKU é obrigatório.')
+            }
+
+            if( !produto.preco || produto.preco <= 0 ){
+                errors.push('O campo preço deve ser maior que zero.')
+            }
+
+            if(!produto.fornecedor){
+                errors.push('O campo fornecedor é obrigatório.')
+            }
+            
+
+            if(errors.length > 0){
+                throw new ErroValidacao(errors)
+            }
+        }
+
+
+        salvar = (produto) => {
+            this.validar(produto)
+
+            let produtos = localStorage.getItem(PRODUTOS)
+            
+            if(!produtos){
+                produtos = []
+            } else {
+                produtos = JSON.parse(produtos)
+            }
+
+            produtos.push(produto);
+
+            localStorage.setItem(PRODUTOS, JSON.stringify(produtos))
+
+            
+        }
+
+}
