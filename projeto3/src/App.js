3073a9ebbc6a65b8064b39dc7fb15c9ad6c5a9ef
F8809379 Ruben Rodrigues Goncalves Dias <rubendias@bb.com.br>
2020-02-11 15:49:07 -0300

    Projeto funcional

---
+++ projeto3/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+
+function App() {
+  
+  const [ numero, setNumero ] = useState()
+  const [ segundoNumero, setSegundoNumero ] = useState()
+  const [ resultado, setResultado ] = useState();
+
+  const somar = () => {
+      const numeroInt = parseInt(numero)
+      const segNumeroInt = parseInt(segundoNumero)
+
+      setResultado( numeroInt + segNumeroInt )
+  }
+
+  return (
+    <div>
+      Número 1: <br />
+      <input type="text" value={numero} 
+             onChange={e => setNumero(e.target.value)} />< br/>
+      Número 2: <br />
+      <input type="text" value={segundoNumero} 
+             onChange={e => setSegundoNumero(e.target.value)} />< br/>
+      <button>Somar</button><br />
+      Resultado: <br />
+      <input type="text" value={resultado}
+             onChange={e => setResultado(e.target.value)} />< br/>
+    </div>
+  );
+}
+
+export default App;
